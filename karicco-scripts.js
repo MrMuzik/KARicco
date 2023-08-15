4288fc2899cbe366d90bba67dff8d4386286415c
Bryan Markham <bmarkham@groupize.com>
2023-08-15 00:08:32 -0400

    adding footer button

--- karicco-scripts.js
+++ karicco-scripts.js
@@ -11,7 +11,7 @@ const init = () => {
   footerRows[0].classList.add('footer-row1');
   footerRows[1].classList.add('footer-row2');
   row1Col2.classList.add('w-cell', 'col', 'col-12', 'col-sm-6', 'col-md-4', 'col-lg-6', 'footer-row1-col2');
-  row1Col2.innerHTML = `<div class="w-container col"><div class="w-cell row"><div class="w-wrapper"><blockquote>This place is great! Personal service and stylists who go out of their way to make your visit convenient and comfortable.&nbsp;<br>-&nbsp;​<a href="https://www.yelp.com/user_details?userid=XiNr6lcIBMpRbj_V64aU-w">Natalie W.</a><br></blockquote></div></div></div>`;
+  row1Col2.innerHTML = `<div class="w-container col"><div class="w-cell row"><div class="w-wrapper"><blockquote>This place is great! Personal service and stylists who go out of their way to make your visit convenient and comfortable.&nbsp;<br>-&nbsp;​<a href="https://www.yelp.com/user_details?userid=XiNr6lcIBMpRbj_V64aU-w">Natalie W.</a><br></blockquote><a type="button" href="https://www.yelp.com/user_details?userid=XiNr6lcIBMpRbj_V64aU-w" class="w-button w-button--button w-button--primary w-button--large w-button--full-width-mobile">More Reviews</a></div></div></div>`;
   footerRows[0].insertBefore(row1Col2, row1Col3);
   row1Col1.classList.remove('col-md-8');
   row1Col1.classList.add('align--left-sm', 'align--left-md', 'align--left-lg', 'col-md-4', 'col-lg-2', 'footer-row1-col1');
