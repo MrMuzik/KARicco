31af3f38e9f4eca789fcee7f95de2b4a47366065
Bryan Markham <bmarkham@groupize.com>
2023-09-17 02:31:58 -0400

    updating script for live 10

--- karicco-scripts.js
+++ karicco-scripts.js
@@ -23,6 +23,7 @@ const homepageGallery = () => {
     const images = gallery.querySelectorAll('img');
     const imageSrcs = [];
     images.forEach((image) => {
+      console.log('imageSrc', image.src);
       imageSrcs.push(image.src);
     });
     rowToReplace.innerHTML = '<div class="local-love-container"><div><img src="https://placehold.co/350x550" width="350" class="imgHasMax" /></div><div class="local-love-centered"><div><img src="https://placehold.co/150x250" width="150" /></div><div class="toBeReplaced">Local Love</div><div class="local-love-social"><a href="https://www.instagram.com/kariccohairdesign/" target="_blank"><img src="https://static.xx.fbcdn.net/rsrc.php/v3/yx/r/tBxa1IFcTQH.png" width="45" height="45" /></a></div><div>Follow Us On Instagram</div><div><img src="https://mrmuzik.github.io/KARicco/images/mermaid.png" width="350" /></div></div><div><img src="https://placehold.co/350x550" width="350" class="imgHasMax" /></div></div>';
